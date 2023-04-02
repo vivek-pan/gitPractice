74d2c166e6a7deba1222eb851cfe0dfb37954220
vivek <vivekpandipalli@gmail.com>
2023-04-02 20:32:20 +0530

    removed spaces

--- src/com/consoleproject/atm/Account.java
+++ src/com/consoleproject/atm/Account.java
@@ -36,11 +36,6 @@ public class Account {
 		return "Account [accountType=" + accountType + ", accountNumber=" + accountNumber + ", accountBalance="
 				+ accountBalance + ", transactions=" + transactions + "]";
 	}
-
-
-
-
-	
 }
 
 
